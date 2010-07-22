7cde9a2f87a102744c585c23cbd203ab443633fb
Felipe Contreras <felipe.contreras@gmail.com>
2010-07-22 22:11:03 +0300

    General cleanups
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- gstavdec.h
+++ gstavdec.h
@@ -1,22 +1,10 @@
 /*
- * Copyright (C) 2009 Nokia Corporation.
+ * Copyright (C) 2009 Felipe Contreras
  *
- * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ * Author: Felipe Contreras <felipe.contreras@gmail.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1.
  */
 
 #ifndef GST_AVDEC_H
@@ -24,8 +12,6 @@
 
 #include <gst/gst.h>
 #include <libavcodec/avcodec.h>
-
-G_BEGIN_DECLS
 
 #define GST_AVDEC(obj) (GstAVDec *) (obj)
 #define GST_AVDEC_TYPE (gst_avdec_get_type())
@@ -61,7 +47,5 @@ struct GstAVDecClass {
 };
 
 GType gst_avdec_get_type(void);
-
-G_END_DECLS
 
 #endif /* GST_AVDEC_H */
