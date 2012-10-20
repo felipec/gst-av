9caca4678d52c0047b4ac194ca146f788ec1ba0e
Felipe Contreras <felipe.contreras@gmail.com>
2012-10-20 15:31:20 +0200

    util: extra safety checks
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- util.c
+++ util.c
@@ -16,11 +16,15 @@
 int64_t gstav_timestamp_to_pts(AVCodecContext *ctx, int64_t ts)
 {
 	AVRational bq = { 1, GST_SECOND * ctx->ticks_per_frame };
+	if (ts == -1)
+		return AV_NOPTS_VALUE;
 	return av_rescale_q(ts, bq, ctx->time_base);
 }
 
 int64_t gstav_pts_to_timestamp(AVCodecContext *ctx, int64_t pts)
 {
 	AVRational bq = { 1, GST_SECOND * ctx->ticks_per_frame };
+	if (pts == (int64_t) AV_NOPTS_VALUE)
+		return -1;
 	return av_rescale_q(pts, ctx->time_base, bq);
 }
